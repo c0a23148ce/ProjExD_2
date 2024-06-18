55fd2041b25286406836c391faec5299175a42c8
C0A23148 <c0a23148ce@edu.teu.ac.jp>
2024-06-18 17:10:28 +0900

    飛ぶ方向に応じてこうかとんを切り替える(途中)

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,9 +10,33 @@ MV_DIC = {  # 移動量を表す辞書
     pg.K_DOWN:(0, +5), 
     pg.K_LEFT:(-5, 0), 
     pg.K_RIGHT:(+5, 0),
+    pg.K_w:(+5, -5),
+    pg.K_q:(-5, -5), 
+    pg.K_a:(-5, +5), 
+    pg.K_s:(+5, +5)
     }
 
+MV_KK = {
+    (+5, -5):135,
+    (-5, -5):315, 
+    (-5, +5):45, 
+    (+5, +5):135,
+    (0, -5):270,
+    (0, +5):90, 
+    (-5, 0):0, 
+    (+5, 0):0
+    }
+
+
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def roto_kk(mv):
+     for k, v in MV_KK.items():
+         if mv is k:
+             return v
+        
+
 
 def check_bound(rct:pg.Rect) -> tuple[bool, bool]:
     """
@@ -26,6 +50,8 @@ def check_bound(rct:pg.Rect) -> tuple[bool, bool]:
     if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
     return yoko, tate
+
+
 
 
 def main():
@@ -42,6 +68,7 @@ def main():
     bom_rct = bom_img.get_rect()
     bom_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
 
@@ -56,12 +83,18 @@ def main():
             
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-    
+        num = 0
+
         for k, v in MV_DIC.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+                num = roto_kk(v)
+            kk_img = pg.transform.flip(kk_img, True, False)
+            kk_img = pg.transform.rotozoom(pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0), num, 1.0)
         kk_rct.move_ip(sum_mv)
+        
+
 
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
